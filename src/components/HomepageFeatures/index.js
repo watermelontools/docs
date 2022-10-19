cebbc71bcec0e659032053f345c7ee94cab4cefc
Esteban Dalel R <estebandalelr@gmail.com>
2022-10-19 10:27:09 -0500

    Fix spacing

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -27,7 +27,7 @@ const FeatureList = [
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx("col col--3")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
